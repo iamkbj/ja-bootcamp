c3191345164e722f6b84bcdd4c142fa3a263d76c
iamkbj <kimbeangjun7@gmail.com>
2021-07-15 23:12:55 +0900

    Updated with strings #10

---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+let firstName = 'Andrew';
+let lastName = 'KBS';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let city = 'Philadelphia'
+let country = 'United States'
+let location = city + ', ' + country
+
+console.log(location);
