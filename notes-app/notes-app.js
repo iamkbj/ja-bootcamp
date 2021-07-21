24e34523a526c98925783a15bb2e0291d9655789
iamkbj <kimbeangjun7@gmail.com>
2021-07-21 21:58:28 +0900

    Update #50

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,29 @@
-console.log('This is from a different file')
+// const notes = [{
+//     title: 'my next trip'
+//     body: 'Iwould like to go to Spain'
+// },{
+//     title:'Habbits to work on'
+//     body:
+// },{
+//     title:
+//     body:
+// }]
+// console.log('This is from a different file')
+
+//Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+//Query all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function(p){
+    p.textContent = '********'
+    //p.remove()
+//    console.log(p.textContent)
+})
+
+// Add a new element 
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
